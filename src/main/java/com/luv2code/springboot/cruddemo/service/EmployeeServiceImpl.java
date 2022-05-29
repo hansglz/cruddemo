1390023da5cb0e181fbf949bb7ec176687c156d4
hansglz <glzhang23@gmail.com>
2022-05-28 19:58:11 -0700

    replace Hibernate/JPA API implementation to Spring Data Api

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.cruddemo.service;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,38 +9,74 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeDAO employeeDAO;
+    // CRUD API with Hibernate/JPA
+//    private EmployeeDAO employeeDAO;
+//
+//    @Autowired
+//    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
+//        this.employeeDAO = employeeDAO;
+//    }
+//
+//    @Override
+//    @Transactional
+//    public List<Employee> findAll() {
+//        return employeeDAO.findAll();
+//    }
+//
+//    @Override
+//    @Transactional
+//    public Employee findById(int id) {
+//        return employeeDAO.findById(id);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void save(Employee employee) {
+//        employeeDAO.save(employee);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void deleteById(int id) {
+//        employeeDAO.deleteById(id);
+//    }
+
+    // Spring Data JPA
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl (EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee employee = null;
+        if (result.isPresent()) employee = result.get();
+        else throw new RuntimeException("Didn't find employee id: " + id);
+
+        return employee;
     }
 
     @Override
-    @Transactional
     public void save(Employee employee) {
-        employeeDAO.save(employee);
+        employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
